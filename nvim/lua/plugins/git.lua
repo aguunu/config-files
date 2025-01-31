9db382a8b290bcd3ed2a0a9286b8c574599cc905
Agustín Núñez <aguunu@hotmail.com>
2025-01-31 12:46:03 -0300

    Add vim-fugitive and gitsigns plugins.

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup({
+                -- watch_gitdir = {
+                --     interval = 1000, -- Check for changes every second
+                --     follow_files = true
+                -- },
+            })
+        end
+    },
+    {
+        'tpope/vim-fugitive'
+    }
+}
